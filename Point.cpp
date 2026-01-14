41130a7249c75ee912f3991eef8963fc2a5ea903
aurora-martinez <aurorareismartinez@icloud.com>
2026-01-14 11:48:58 -0500

    getdistance function (not done)

--- Point.cpp
+++ Point.cpp
@@ -4,7 +4,17 @@ Point::Point(int L, double l)
     :longitude(L), latitude(l) {}
 
 Point::getdistance(Point p1, Point p2) {
+    double rad1long = p1.longitude*(M_PI/180);
+    double rad2long = p2.longitude*(M_PI/180);
 
+    double rad1lat = p1.latitude*(M_PI/180);
+    double rad2lat = p2.latitude*(M_PI/180);
 
-    return 0;
+    double Dlong = rad2long - rad1long;
+    double Dlat = rad2lat - rad1lat;
+
+    double a = ((std::sin(Dlat/2))**2) + (std::cos(rad1lat) * std::cos(rad2lat) * (std::sin(Dlong/2))**2)
+    double c = 2* (a * tan2(std::sqrt(a), std::sqrt(1-a))) ;
+    double distance = 6731 * c;
+    return distance;
 }
